0649129022899e345794286b8a52b9eb8b0b20b0
yaoone <729299641@qq.com>
2019-10-31 10:58:38 +0800

    day5

---
+++ day7/my-project/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <Search />
+    <Grid />
+    <Divider />
+    <Card/>
+    <Tabbar />
+  </div>
+</template>
+
+<script>
+import Search from "./components/Search.vue";
+import Grid from "./components/Grid.vue";
+import Divider from "./components/Divider.vue";
+import Card from "./components/Card.vue";
+import Tabbar from "./components/Tabbar.vue";
+
+export default {
+  name: "app",
+  components: {
+    Search,
+    Grid,
+    Divider,
+    Card,
+    Tabbar
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ day7/my-project/src/components/Tabbar.vue
@@ -0,0 +1,20 @@
+<template>
+  <van-tabbar v-model="active">
+    <van-tabbar-item icon="home-o">标签</van-tabbar-item>
+    <van-tabbar-item icon="search">标签</van-tabbar-item>
+    <van-tabbar-item icon="friends-o">标签</van-tabbar-item>
+    <van-tabbar-item icon="setting-o">标签</van-tabbar-item>
+  </van-tabbar>
+</template>
+<script>
+import Vue from "vue";
+import { Tabbar, TabbarItem } from "vant";
+Vue.use(Tabbar).use(TabbarItem);
+export default {
+  data() {
+    return {
+      active: 0
+    };
+  }
+};
+</script>
